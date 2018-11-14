841a8181db8535e65cdee6ce3f0bf36311360716
kpeenee <43548093+kpeenee@users.noreply.github.com>
2018-11-14 09:52:00 +0000

    Zombie spawner

---
+++ Gloo/Assets/Scripts/SpawnZombie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZombie : MonoBehaviour {
+    public float timeBtwSpawn = 0;
+    public float startTimeBtwSpawn = 5;
+    public GameObject zombie;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeBtwSpawn <= 0)
+        {
+            Instantiate(zombie, gameObject.transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+	}
+}
