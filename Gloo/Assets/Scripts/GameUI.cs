d7488696da5740b8688c75c644e8e549987937da
kpeenee <43548093+kpeenee@users.noreply.github.com>
2018-11-14 11:52:47 +0000

    Score update

--- Gloo/Assets/Scripts/GameUI.cs
+++ Gloo/Assets/Scripts/GameUI.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour {
     public static int playerScore = 0;
+    public static int playerHealth = 100;
     public Text scoreText;
+    public Slider healthBar;
+
     public void Update()
     {
-            scoreText.text = "SCORE: " + playerScore;
+        playerHealth -= 1;
+        scoreText.text = "SCORE: " + playerScore;
+        healthBar.value = playerHealth;
     }
 
 }
