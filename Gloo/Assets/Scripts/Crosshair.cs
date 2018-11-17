9edad334d2ad5b809dbfe25def1afa7d9f6aa600
kpeenee <43548093+kpeenee@users.noreply.github.com>
2018-11-17 22:52:38 +0000

    Crosshair

---
+++ Gloo/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+    public Transform crosshair;
+    public Sprite idle;
+    public Sprite shoot;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        crosshair.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - new Vector3 (0,0,-10);
+       if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<SpriteRenderer>().sprite = shoot;
+            Invoke("BackToIdle", 0.2f);
+        }
+        
+	}
+
+    void BackToIdle()
+    {
+        GetComponent<SpriteRenderer>().sprite = idle;
+    }
+    
+}
