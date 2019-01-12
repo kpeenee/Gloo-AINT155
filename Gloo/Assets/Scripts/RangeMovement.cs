8d35d5a60fcd731764c17ada8e00b3c217d4254a
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-01-12 16:47:25 +0000

    Demon animation

--- Gloo/Assets/Scripts/RangeMovement.cs
+++ Gloo/Assets/Scripts/RangeMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RangeMovement : MonoBehaviour {
+    private Animator anim;
 
     public float speed;
     public float stoppingDistance;
@@ -15,6 +16,7 @@ public class RangeMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -22,14 +24,17 @@ public class RangeMovement : MonoBehaviour {
         if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            anim.SetBool("isMoving", true);
         }
         else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && (Vector2.Distance(transform.position, player.position) > retreatDistance))
         {
             transform.position = this.transform.position;
+            anim.SetBool("isMoving", false);
         }
         else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            anim.SetBool("isMoving", true);
         }
 
     }
