77a8884699565cb7df73e2665a9f26eb1d5108df
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-01-13 12:34:37 +0000

    Power up added

---
+++ Gloo/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    public float duration = 5.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(FireRateIncrease(collision));
+        }
+    }
+
+    IEnumerator FireRateIncrease(Collider2D player)
+    {
+        PlayerShoot shoot = player.GetComponent<PlayerShoot>();
+        shoot.startTimeBtwShots = 0.1f;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        shoot.startTimeBtwShots = 0.5f;
+
+        Destroy(gameObject);
+    }
+}
